cb1a5664bf47aca6b6d62afa7f851f05a59ee456
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-12-06 11:56:28 -0300

    Havia cometido um erro no redirecionamento após login bem sucedido

--- src/app/(auth)/login/_actions/login.ts
+++ src/app/(auth)/login/_actions/login.ts
@@ -1,30 +1,30 @@
-'use server'
+'use server';
 import { AuthError } from 'next-auth';
 import { redirect } from 'next/navigation';
-import { signIn } from "auth";
+import { signIn } from 'auth';
 
-export default async function login(FormData: FormData){
+export default async function login(FormData: FormData) {
   const entries = Array.from(FormData.entries());
-  const {email, password} = Object.fromEntries(entries) as {
-    email: string
-    password: string 
-  }
+  const { email, password } = Object.fromEntries(entries) as {
+    email: string;
+    password: string;
+  };
   try {
     await signIn('credentials', {
       email,
       password,
+      redirect: false
     });
+    redirect('/');
   } catch (error) {
     if (error instanceof AuthError) {
       if (error.type === 'CredentialsSignin') {
         throw new Error('Credenciais inválidas');
       }
       if (error.type === 'CallbackRouteError') {
-        throw new Error('Ocorreu um erro ao validar as informações.')
+        throw new Error('Ocorreu um erro ao validar as informações.');
       }
     }
-    throw error
+    throw error;
   }
-
-  redirect('/');
 }
